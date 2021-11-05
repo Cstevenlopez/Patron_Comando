8100c5a7425acdec33c6d62ea00be6e23caff0c8
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-11-04 20:22:01 -0600

    Inicializacion del proyecto

---
+++ src/main/java/org/steven/App.java
@@ -0,0 +1,13 @@
+package org.steven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
